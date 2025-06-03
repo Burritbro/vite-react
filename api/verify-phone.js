8940f12bccb86ccfacd3d4e58e3ddd533d434c29
Trevor Cole <108247464+Burritbro@users.noreply.github.com>
2025-06-02 21:53:34 -0400

    Update verify-phone.js

--- api/verify-phone.js
+++ api/verify-phone.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   const { number } = req.query;
 
-  const url = `http://apilayer.net/api/validate?access_key=bc1286a36597213e9e0f3d16d8642598&number=${number}&country_code=US`;
+  const url = const url = `https://apilayer.net/api/validate?access_key=bc1286a36597213e9e0f3d16d8642598&number=${number}&country_code=US`;
+
 
   try {
     const response = await fetch(url);
