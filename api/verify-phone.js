8963020a48ad42106770bf5aaa69fe7fb1709d1e
Trevor Cole <108247464+Burritbro@users.noreply.github.com>
2025-06-02 21:42:38 -0400

    Create verify-phone.js

---
+++ api/verify-phone.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { number } = req.query;
+
+  const url = `http://apilayer.net/api/validate?access_key=bc1286a36597213e9e0f3d16d8642598&number=${number}&country_code=US`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to validate phone number" });
+  }
+}
